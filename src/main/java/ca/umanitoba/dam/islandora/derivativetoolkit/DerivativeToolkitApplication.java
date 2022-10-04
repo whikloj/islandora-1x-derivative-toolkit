6082e0ffd2108aa346d13b0da486009a190c47d4
Jared Whiklo <jwhiklo@gmail.com>
2022-10-04 15:40:33 -0500

    Remove version file needed for picocli, add property check in application

--- src/main/java/ca/umanitoba/dam/islandora/derivativetoolkit/DerivativeToolkitApplication.java
+++ src/main/java/ca/umanitoba/dam/islandora/derivativetoolkit/DerivativeToolkitApplication.java
@@ -1,6 +1,9 @@
 package ca.umanitoba.dam.islandora.derivativetoolkit;
 
+import static ca.umanitoba.dam.islandora.derivativetoolkit.DefaultConfig.TOOLKIT_CONFIG_PROPERTY;
 import static org.slf4j.LoggerFactory.getLogger;
+
+import java.io.File;
 
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +15,18 @@ public class DerivativeToolkitApplication {
 	private static final Logger LOGGER = getLogger(DerivativeToolkitApplication.class);
 
 	public static void main(String[] args) {
+		final String prop = System.getProperty(TOOLKIT_CONFIG_PROPERTY, null);
+		if (prop == null) {
+			System.out.println("You need to specify the location of the configuration file with -Dfc3indexer.config" +
+					".file=");
+			return;
+		} else {
+			final File propFile = new File(prop);
+			if (!(propFile.exists() || propFile.canRead())) {
+				System.out.println("Property file " + prop + " is not a readable file.");
+				return;
+			}
+		}
 		SpringApplication.run(DerivativeToolkitApplication.class, args);
 	}
 }
